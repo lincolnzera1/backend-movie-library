9875dc837e419dd70a94369400a91546b1b82e2b
lincolnzera1 <guilherme.lincoln.lima.11@gmail.com>
2023-12-11 11:06:32 -0300

    Implement CRUD operations for users

--- movielibrary/src/main/java/com/movielibrary/movielibrary/MovielibraryApplication.java
+++ movielibrary/src/main/java/com/movielibrary/movielibrary/MovielibraryApplication.java
@@ -2,8 +2,9 @@ package com.movielibrary.movielibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MovielibraryApplication {
 
 	public static void main(String[] args) {
