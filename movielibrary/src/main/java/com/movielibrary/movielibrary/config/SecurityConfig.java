c5b8755bf22b7e7d7341936fe5dc66590adfd944
lincolnzera1 <guilherme.lincoln.lima.11@gmail.com>
2023-12-12 05:11:33 -0300

    Adjustments in Swagger

--- movielibrary/src/main/java/com/movielibrary/movielibrary/config/SecurityConfig.java
+++ movielibrary/src/main/java/com/movielibrary/movielibrary/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
+    // Criptografação de senhas
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
